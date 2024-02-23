1176a61197e806318badc8cbd830bb2941034eea
Kunal Jadhav <127647392+Kunal-here@users.noreply.github.com>
2024-02-23 16:23:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,83 +4,7 @@ const nav = document.querySelector('.nav')
 const logo = document.querySelector(".logo")
 const cursor = document.querySelector(".cursor")
 const body = document.querySelector("body");
-
-
-
-const coords = { x: 0, y: 0 };
-const circles = document.querySelectorAll(".circle");
-
-const colors = [
-//   "#ffb56b",
-//   "#fdaf69",
-//   "#f89d63",
-//   "#f59761",
-//   "#ef865e",
-//   "#ec805d",
-//   "#e36e5c",
-//   "#df685c",
-//   "#d5585c",
-//   "#d1525c",
-//   "#c5415d",
-//   "#c03b5d",
-//   "#b22c5e",
-//   "#ac265e",
-//   "#9c155f",
-//   "#950f5f",
-//   "#830060",
-//   "#7c0060",
-//   "#680060",
-//   "#60005f",
-//   "#48005f",
-//   "#3d005e"
-];
-
-circles.forEach(function (circle, index) {
-  circle.x = 0;
-  circle.y = 0;
-  circle.style.backgroundColor = colors[index % colors.length];
-});
-
-
-
-window.addEventListener("mousemove", function(e){
-  coords.x = e.clientX;
-  coords.y = e.clientY;
-  
-});
-
-function animateCircles() {
-  
-  let x = coords.x;
-  let y = coords.y;
-  
-  circles.forEach(function (circle, index) {
-    circle.style.left = x - 12 + "px";
-    circle.style.top = y - 12 + "px";
-    
-    circle.style.scale = (circles.length - index) / circles.length;
-    
-    circle.x = x;
-    circle.y = y;
-
-    const nextCircle = circles[index + 1] || circles[0];
-    x += (nextCircle.x - x) * 0.3;
-    y += (nextCircle.y - y) * 0.3;
-  });
- 
-  requestAnimationFrame(animateCircles);
-}
-
-animateCircles();
-
-
-
-body.addEventListener("mousemove",(dets)=>{
-    cursor.style.left = dets.x-10+"px"
-    cursor.style.top = dets.y-10+"px"
-})
-
-
+const main = document.querySelector(".main");
 
 
 hamburger.addEventListener("click", () => {
@@ -94,11 +18,14 @@ hamburger.addEventListener("click", () => {
       nav.style.backgroundColor = "black";
       logo.style.color = "white";
       isBlack = true;
+      hamburger.classList.replace("fa-bars", "fa-x");
     } else {
       nav.style.backgroundColor = "transparent";
       isBlack = false;
       logo.style.color = "black";
+      hamburger.classList.replace("fa-x", "fa-bars");
     }
+
   });
 
 
@@ -151,7 +78,7 @@ gsap.timeline()
         x:300,
         opacity:0,
         duration:0.5,
-        ease: "back"
+        ease: "back",
     }).from(".intro h2",{
         y:0,
         x:300,
@@ -171,6 +98,11 @@ gsap.timeline()
         duration:0.3,
         ease: "back"
     })
+
+
+// 
+
+
 
 
 
